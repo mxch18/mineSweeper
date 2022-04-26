7a6ab5f2ef9c0bfe6255d7db30585611cf634441
mxch18 <maxens.achiepi@gmx.com>
2022-04-26 16:03:41 +0200

    [FEAT] - Add main game and information frames + add separate settings file

--- mineSweeper.py
+++ mineSweeper.py
@@ -1,5 +1,38 @@
 import tkinter
+import math
+import settings
 
 root = tkinter.Tk()
 
+# Window settings
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
+root.configure(bg='black')
+root.title('Mine Sweeper Game')
+root.resizable(False, False)
+
+root.update()
+
+# Main game frame
+frame_mine = tkinter.Frame(
+    root,
+    bg='red',
+    width=math.floor(root.winfo_width()*0.75),
+    height=root.winfo_height()
+)
+frame_mine.place(x=0, y=0)
+frame_mine.update()
+
+# Information frame
+frame_information = tkinter.Frame(
+    root,
+    bg='blue',
+    width=root.winfo_width()-frame_mine.winfo_width(),
+    height=root.winfo_height()
+)
+frame_information.place(
+    x=frame_mine.winfo_width(),
+    y=0
+)
+
+# Run the window
 root.mainloop()
