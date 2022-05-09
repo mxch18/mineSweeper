2440e38f98809b75246e45c47090efc163199ca3
mxch18 <maxens.achiepi@gmx.com>
2022-05-09 12:19:50 +0200

    [REFACTOR] - Change camel case to snake case for variables and class methods

--- cell.py
+++ cell.py
@@ -2,39 +2,39 @@ import tkinter
 
 
 class Cell(tkinter.Button):
-    def __init__(self, gridRow, gridCol, *args, **kwargs):
+    def __init__(self, grid_row, grid_col, *args, **kwargs):
         tkinter.Button.__init__(self, *args, **kwargs)
-        self.grid(row=gridRow, column=gridCol)
-        self.bind('<Button-1>', self.cmdLeftClickBtn)
-        self.bind('<Button-3>', self.cmdRightClickBtn)
+        self.grid(row=grid_row, column=grid_col)
+        self.bind('<Button-1>', self.cmd_left_click_btn)
+        self.bind('<Button-3>', self.cmd_right_click_btn)
 
-        self.isMine = False
-        self.isFlag = False
+        self.is_mine = False
+        self.is_flag = False
 
-    def cmdLeftClickBtn(self, event):
-        infoBtn = self.grid_info()
+    def cmd_left_click_btn(self, event):
+        info_btn = self.grid_info()
 
-        if infoBtn['in'].isFirstClick:
+        if info_btn['in'].is_first_click:
             # Create minefield
-            infoBtn['in'].createMinefield(
-                *[infoBtn.get(key) for key in ('row', 'column')]
+            info_btn['in'].createMinefield(
+                *[info_btn.get(key) for key in ('row', 'column')]
             )
 
-        infoBtn['in'].mineSweep(
-            *[infoBtn.get(key) for key in ('row', 'column')]
+        info_btn['in'].mineSweep(
+            *[info_btn.get(key) for key in ('row', 'column')]
         )
 
-    def cmdRightClickBtn(self, event):
-        infoBtn = self.grid_info()
+    def cmd_right_click_btn(self, event):
+        info_btn = self.grid_info()
         # Set image to flag, or remove flag
-        if self.isFlag:
-            self.configure(image=infoBtn['in'].imageDict['0'])
-            infoBtn['in'].decreaseNbOfFlagsUsed()
-            self.isFlag = 0
-        elif self['state'] == tkinter.ACTIVE and infoBtn['in'].isEnoughFlags():
-            self.configure(image=infoBtn['in'].imageDict['flag'])
-            infoBtn['in'].increaseNbOfFlagsUsed()
-            self.isFlag = 1
+        if self.is_flag:
+            self.configure(image=info_btn['in'].image_dict['0'])
+            info_btn['in'].decrease_nb_of_flags_used()
+            self.is_flag = 0
+        elif self['state'] == tkinter.ACTIVE and info_btn['in'].is_enough_flags():
+            self.configure(image=info_btn['in'].image_dict['flag'])
+            info_btn['in'].increase_nb_of_flags_used()
+            self.is_flag = 1
 
-    def setMine(self):
-        self.isMine = 1
+    def set_mine(self):
+        self.is_mine = 1
