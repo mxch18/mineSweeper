c6e658e2c4e7b96959d99c1b882f1675bbda3b8f
mxch18 <maxens.achiepi@gmx.com>
2022-05-09 12:22:13 +0200

    [REFACTOR] - Change camel case to snake case

--- gamePage.py
+++ gamePage.py
@@ -3,32 +3,32 @@ from infoPage import InfoPage
 
 
 class GamePage():
-    def __init__(self, nbCol_, nbRow_, nbMines_, root_, *args, **kwargs):
-        self.cells = CellGrid(nbCol=nbCol_, nbRow=nbRow_, nbMines=nbMines_,
+    def __init__(self, nb_col_, nb_row_, nb_mines_, root_, *args, **kwargs):
+        self.cells = CellGrid(nb_col=nb_col_, nb_row=nb_row_, nb_mines=nb_mines_,
                               master=root_, *args, **kwargs)
         self.cells.update()
 
         self.infos = InfoPage(self.cells.winfo_width(), '', root_)
-        self.infos.setCmdBtn('restart', self.resetCellGrid)
-        self.infos.setCmdBtn('change_diff', self.returnToMenu)
+        self.infos.setCmdBtn('restart', self.reset_cell_grid)
+        self.infos.setCmdBtn('change_diff', self.return_to_menu)
         self.infos.setTextvariableLabel(self.cells.nbOfFlagsUsed)
 
-    def resetCellGrid(self):
+    def reset_cell_grid(self):
         # Get parent
         parent = self.cells.nametowidget(self.cells.winfo_parent())
         # Destroy widget and all children
         self.cells.destroy()
         # Recreate widget
         self.cells = CellGrid(
-            nbCol=self.cells.nbCol,
-            nbRow=self.cells.nbRow,
-            nbMines=self.cells.nbMines,
+            nb_col=self.cells.nb_col,
+            nb_row=self.cells.nb_row,
+            nb_mines=self.cells.nb_mines,
             master=parent,
             height=parent.winfo_height(),
             width=parent.winfo_width()
         )
 
-    def returnToMenu(self):
+    def return_to_menu(self):
         self.cells.destroy()
         self.infos.destroy()
 
